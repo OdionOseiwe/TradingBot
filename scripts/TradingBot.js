b03909c7655301f19efdf24a491b627c3dc4ef80
OdionOseiwe <oseiweifebhor@gmail.com>
2023-07-28 23:56:21 +0100

    ...

--- scripts/TradingBot.js
+++ scripts/TradingBot.js
@@ -12,6 +12,7 @@ const PORT = process.env.PORT || 5000
 const app = express();
 const server = http.createServer(app).listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
+const signer = provider.getSigner();
 
 
 
