c740db334239a24ffebd5d91373807b1a782b757
OdionOseiwe <oseiweifebhor@gmail.com>
2023-07-29 00:34:42 +0100

    ...

--- scripts/TradingBot.js
+++ scripts/TradingBot.js
@@ -1,10 +1,9 @@
 require('dotenv').config()
 const express = require('express')
 const http = require('http')
-var {ethers, JsonRpcProvider} = require('ethers');
+const ethers = require('ethers');
 const url = "https://mainnet.infura.io/v3/e7af8fc766b941c498bc770af5246568";
 const provider = new ethers.providers.JsonRpcProvider(url);
-
 
 
 // Setting up the server for the bot to listen from
@@ -14,7 +13,16 @@ const server = http.createServer(app).listen(PORT, () => console.log(`Listening 
 
 const signer = provider.getSigner();
 
+// uniswap v3 goerli address 0x1F98431c8aD98523631AE4a59f267346ea31F984
 
+// goerli DAI
+const DAI_ABI = [{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"INITIAL_SUPPLY","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_value","type":"uint256"}],"name":"burn","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_value","type":"uint256"}],"name":"burnFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"_name","type":"string"},{"name":"_symbol","type":"string"},{"name":"_decimals","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_burner","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Burn","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"}]
+const DAI_ADDRESS = '0x72560B830CeD423FBb9Ec1ae8d01B41F015a5F21'
+const daiContract = new web3.eth.Contract(DAI_ABI, DAI_ADDRESS);
+
+const UNISWAP_ROUTER = ''
+const EXCHANGE_ADDRESS = '0x1F98431c8aD98523631AE4a59f267346ea31F984'
+const exchangeContract = new web3.eth.Contract(UNISWAP_ROUTER, EXCHANGE_ADDRESS);
 
 
 
